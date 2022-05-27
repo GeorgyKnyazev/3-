055dc442c5e4067144154528cd591598423b0482
Георгий Князев <saint_georgy@yahoo.com>
2022-05-27 10:13:32 +0300

    Добавьте файлы проекта.

---
+++ 3 задание/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _3_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как вас зовут?");
+            string clientName = Console.ReadLine();
+            Console.WriteLine("Кто вы по гороскопу?");
+            string clientZodiacSing = Console.ReadLine();
+            Console.WriteLine("Сколько вам лет?");
+            string clientAge = Console.ReadLine();
+            Console.WriteLine("Где вы работаете?");
+            string clientWork = Console.ReadLine();
+            Console.WriteLine("Вас зовут " + clientName + ", вам " + clientAge + ", ваш знак зодиака " + clientZodiacSing + ", вы работаете " + clientWork);
+        }
+    }
+}
